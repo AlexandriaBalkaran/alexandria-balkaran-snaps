e99ad740b049f8575b853cb04ef17037b825535a
Alexandria Balkaran <alexandriabalkaran@gmail.com>
2025-02-18 21:46:39 -0500

    created homepage, run axios

--- alexandria-balkaran-snaps/src/App.jsx
+++ alexandria-balkaran-snaps/src/App.jsx
@@ -1,54 +1,20 @@
 import React, { useState } from "react";
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
-import FilterButton from "./components/FilterButton/FilterButton";
-import FilterDrawer from "./components/FilterDrawer/FilterDrawer";
-import FilteredPhotos from "./components/FilteredPhotos/FilteredPhotos";
-import Footer from "./components/Footer/Footer";
+// import Header from "./components/Header/Header";
+// import Footer from "./components/Footer/Footer";
 import photos from "./data/photos.json";
 import "./App.scss";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
-  const [selectedTag, setSelectedTag] = useState(null);
-  const [drawerOpen, setDrawerOpen] = useState(false);
-  const [isActive, setIsActive] = useState(false);
-
-  const filteredPhotos = selectedTag
-    ? photos.filter((photo) => photo.tags.includes(selectedTag))
-    : photos;
-
-  const handleButtonClick = () => {
-    setIsActive(!isActive);
-    setDrawerOpen(!drawerOpen);
-  };
 
   return (
-    <div className="app">
-      <div className="header--fixed">
-        <Header />
-        <FilterButton
-          isActive={isActive}
-          drawerOpen={drawerOpen}
-          handleButtonClick={handleButtonClick}
-        />
-      </div>
-
-      <div className="main__content">
-        <div className={`filter__drawer ${drawerOpen ? 'filter__drawer--open' : ''}`}>
-          <FilterDrawer
-            drawerOpen={drawerOpen}
-            selectedTag={selectedTag}
-            setSelectedTag={setSelectedTag}/>
-        </div>
-
-        <div className="hero-photos__content">
-          <Hero />
-          <FilteredPhotos filteredPhotos={filteredPhotos} />
-        </div>
-      </div>
-
-      <Footer />
-    </div>
+    <>
+      {/* <Header /> */}
+      <HomePage></HomePage>
+      {/* <Hero /> */}
+      {/* <Footer /> */}
+    </>
+  
   );
 }
 
